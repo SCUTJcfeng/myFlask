5df694438a4e50df3f888d91e4c63bb6b6333828
jc <scutjcfeng@gmail.com>
2018-03-09 23:36:28 +0800

    Add login features.

--- page.py
+++ page.py
@@ -1,8 +1,21 @@
 from flask import Flask
 from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+from flask import flash, redirect, url_for
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '3ef6ffg4'
 
+@app.route('/home',methods=['get'])
 @app.route('/',methods=['get'])
 def home():
     user = {'nickname':'Jcfeng'} # fake user
@@ -21,9 +34,13 @@ def home():
                            user = user,
                            posts = posts)
 
-@app.route('/login',methods=['get'])
+@app.route('/login',methods=['get', 'post'])
 def login():
-    return 'login'
+    form = LoginForm()
+    if form.validate_on_submit(): # post && everything is all right it will return True
+        flash('Login requested for user={}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Sign In', form=form)
 
 
 if __name__ == '__main__':
